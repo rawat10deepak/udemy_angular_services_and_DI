6c75672c07cdee33f71cd4de813278f0eff56864
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-05 13:15:21 +0530

    removed account service from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
-import { AccountsService } from './shared/accounts.service';
 import { LoggingService } from './shared/logging.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [AccountsService, LoggingService],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [LoggingService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
